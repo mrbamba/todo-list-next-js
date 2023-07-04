454cfc8c96b096d717de6398a6b63cef427e7d7e
Dan Haski <dhaski@gmail.com>
2023-07-04 13:02:37 +0300

    Reorganization and consolidation

---
+++ src/app/page.tsx
@@ -0,0 +1,50 @@
+import { Header } from "@/src/components/Header"
+import { TodoItem } from "@/src/components/TodoItem"
+import { prisma } from "@/db"
+import Link from "next/link"
+
+function getTodos(){
+  return prisma.todo.findMany()
+}
+
+async function toggleTodo(id: string, complete: boolean){
+  "use server"
+  console.log("testsing server connection", id, complete)
+  await prisma.todo.update({
+    where: {
+      id: id,
+    },
+    data:{
+      complete:complete,
+    }
+  })
+}
+
+async function deleteTodo(id: string){
+  "use server"
+  console.log("deleting Todo:", id)
+  await prisma.todo.delete({
+    where: {
+      id: id,
+    }
+  })
+}
+
+export default async function Home(){
+  const todos = await  getTodos()
+
+  return (
+    <>
+      <Header/>
+      <ul className="pl-4">
+        {todos.map(todo=>(
+          <TodoItem 
+          key={todo.id} 
+          {...todo} 
+          toggleTodo={toggleTodo} 
+          deleteTodo={deleteTodo}/>
+        ))}
+      </ul>
+    </>
+)
+}
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,30 @@
+"use client"
+type TodoItemProps = {
+    id: string
+    title: string
+    complete: boolean
+    toggleTodo:(id:string, complete:boolean)=>void
+    deleteTodo:(id:string)=>void
+}
+
+export function TodoItem({id, title, complete, toggleTodo, deleteTodo}:TodoItemProps){
+    return <li className="flex flex-1 items-center">
+        <input 
+        id={id} 
+        type="checkbox" 
+        className="cursor-pointer peer flex-initial mr-4" 
+        defaultChecked={complete} 
+        onChange={e=>toggleTodo(id,e.target.checked)}/>
+        <label 
+        htmlFor={id} 
+        className="cursor-pointer peer-checked:line-through 
+        peer-checked:text-slate-500 flex-1"
+        >
+            {title}
+        </label>
+        <button onClick={()=>deleteTodo(id)} 
+        className="flex-initial text-red-600 hover:text-red-600/75">
+            Delete
+        </button>
+    </li>
+}
